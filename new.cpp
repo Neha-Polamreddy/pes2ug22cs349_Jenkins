95eff5f59bd6a45a0e6f31978b5d95ce4e261a82
Neha Polamreddy <116265299+Neha-Polamreddy@users.noreply.github.com>
2025-03-18 10:07:55 +0530

    Create new.cpp

---
+++ new.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+int main() {
+    std::cout << "Hello, world!" << std::endl;
+    std::cout << "Welcome to jenkinsfile." << std::endl;
+    std::cout << "This is a simple print statement!!!" << std::endl;
+    return 0;
+}
